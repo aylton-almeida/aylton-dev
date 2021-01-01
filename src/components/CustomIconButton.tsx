50d897a8d3b661ea151d42db1bfa38ba6e602977
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-01-01 16:24:58 -0300

    icon

--- src/components/CustomIconButton.tsx
+++ src/components/CustomIconButton.tsx
@@ -1,4 +1,4 @@
-import { FC, forwardRef } from "react";
+import { FC, ForwardedRef, forwardRef } from "react";
 
 import { Container } from "@styles/components/CustomIconButton";
 import { Tooltip, Typography } from "@material-ui/core";
@@ -13,21 +13,26 @@ interface CustomIconButtonProps {
 
 const CustomIconButton: FC<CustomIconButtonProps> = forwardRef(
   ({ title, color, showLabel, hideTooltip, children, onClick }, ref) => {
-    return (
-      <Tooltip
-        title={title || ""}
-        ref={ref}
-        open={hideTooltip || !title ? false : undefined}
+    const IconButton = (ref: ForwardedRef<unknown>) => (
+      <Container
+        color={color}
+        disabled={!onClick}
+        onClick={onClick}
+        ref={ref as any}
       >
-        <Container color={color} disabled={!onClick} onClick={onClick}>
-          {children}
-          {showLabel && (
-            <Typography variant="button" className="title">
-              {title}
-            </Typography>
-          )}
-        </Container>
-      </Tooltip>
+        {children}
+        {showLabel && (
+          <Typography variant="button" className="title">
+            {title}
+          </Typography>
+        )}
+      </Container>
+    );
+
+    return hideTooltip ? (
+      IconButton(ref)
+    ) : (
+      <Tooltip title={title || ""}>{IconButton(ref)}</Tooltip>
     );
   }
 );
